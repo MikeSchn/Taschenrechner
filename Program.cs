873252d1a2c3c7e71b49dbcaef22dc9858673485
Mike Schneider <obholzdesign@gmail.com>
2020-05-31 04:49:03 +0200

    Addieren Funktion von Int in Float Geändert

--- Program.cs
+++ Program.cs
@@ -13,14 +13,14 @@ namespace Taschenrechner
         {
             Console.Write("Gieb den ersten Summanden ein:");
             string zahl1 = Console.ReadLine();
-            int zahlA = int.Parse(zahl1);
+            float zahlA = Convert.ToSingle(zahl1); // Konvertiert String in Gleitkommazahl
 
             Console.Write("Gieb den zweiten Summanden ein:");
             string zahl2 = Console.ReadLine();
-            int zahlB = int.Parse(zahl2);
+            float zahlB = Convert.ToSingle(zahl2); // Konvertiert String in Gleitkommazahl
 
 
-            int summe = zahlA + zahlB;
+            float summe = zahlA + zahlB;
 
             Console.WriteLine("Die Addition von: " + zahl1 + " + " + zahl2 + " = {0}", summe);
             Console.Write("Zum Beenden eine Taste Drücken!");
