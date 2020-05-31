02b4704a1db8f8b015c3dbe6bb810b15664d2fd7
Mike Schneider <obholzdesign@gmail.com>
2020-05-31 23:12:34 +0200

    Methode Multiplizieren und Dividieren hinzugefügt

--- Program.cs
+++ Program.cs
@@ -22,12 +22,26 @@ namespace Taschenrechner
             if (operation == "+")
             {
                 double summe = Addiere(zahlA, zahlB);
-                Console.WriteLine("Das Ergebnis der Addition ist: {0}", summe);
+                Console.WriteLine("Das Ergebnis der Addition(Summe) ist: {0}", summe);
             }
             else if (operation == "-")
             {
                 double summe = Subtrahiere(zahlA, zahlB);
-                Console.WriteLine("Das Ergebnis der Subtraction ist: {0}", summe);
+                Console.WriteLine("Das Ergebnis der Subtraktion(Differenz) ist: {0}", summe);
+            }
+            else if (operation == "*")
+            {
+                double summe = Multiplizieren(zahlA, zahlB);
+                Console.WriteLine("Das Ergebnis der Multiplikation(Produkt) ist: {0}", summe);
+            }
+            else if (operation == "/")
+            {
+                double summe = Dividieren(zahlA, zahlB);
+                Console.WriteLine("Das Ergebnis der Division(Quotient) ist: {0}", summe);
+            }
+            else
+            {
+                Console.WriteLine("Die eingabe war Falsch");
             }
 
             BenutzerEingabe("Zum Beenden eine Taste Drücken!");
@@ -40,16 +54,29 @@ namespace Taschenrechner
 
             return number;
         }
+
         static double Addiere(double ersterSummand, double zweiterSummand) 
         {
             double summe = ersterSummand + zweiterSummand;
             return summe;
         }
+
         static double Subtrahiere(double minuend, double subtrahent)
         {
             double summe = minuend - subtrahent;
             return summe;
         }
+
+        static double Multiplizieren(double multiplikant, double multiplikator)
+        {
+            double summe = multiplikant * multiplikator;
+            return summe;
+        }
         
+        static double Dividieren(double dividend, double divisor)
+        {
+            double summe = dividend / divisor;
+            return summe;
+        }
     }
 }
